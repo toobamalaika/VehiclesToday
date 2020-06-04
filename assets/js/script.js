479e31748f16b876a32a2fed3919aad138f3fe96
tooba-malaika <tooba.malaika@sapphire.co>
2020-06-04 22:55:20 +0500

    Add Index Page

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+    "use strict";
+    $(".car-1-data").click(function () {
+      $(".car-1").toggleClass("pCard_on");
+      $(".car-1-data i").toggleClass("fa-minus");
+    });
+
+    $(".car-2-data").click(function () {
+      $(".car-2").toggleClass("pCard_on");
+      $(".car-2-data i").toggleClass("fa-minus");
+    });
+
+    $(".car-3-data").click(function () {
+      $(".car-3").toggleClass("pCard_on");
+      $(".car-3-data i").toggleClass("fa-minus");
+    });
+
+    $(".car-4-data").click(function () {
+      $(".car-4").toggleClass("pCard_on");
+      $(".car-4-data i").toggleClass("fa-minus");
+    });
+  });
